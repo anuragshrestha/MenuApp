4ca6fb9a92f70713a1b47607f6fa03e5d16fb754
Anurag Shrestha <ashrestha@unm.edu>
2024-01-28 13:38:23 -0700

    updated App file

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hy</Text>
+      <Text>Okay </Text>
       <StatusBar style="auto" />
     </View>
   );
