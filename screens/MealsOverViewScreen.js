ef2c21140d897310b4f95fc0aac2aaea0ffe2c6a
Anurag Shrestha <ashrestha@unm.edu>
2024-01-29 21:19:01 -0700

    added Meals overview screen

---
+++ screens/MealsOverViewScreen.js
@@ -0,0 +1,19 @@
+
+import { Text, View, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+
+ function MealsOverViewScreens(){
+    return(
+        <Text>Meals OverView screen!</Text>
+    )
+ }
+
+
+ const style = StyleSheet.create({
+    conatiner:{
+        flex: 1,
+    }
+ })
+   
+ export default MealsOverViewScreens;
