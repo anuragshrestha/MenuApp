bd39a64547625393668df2b4a79fbff96d6e8d19
Anurag Shrestha <ashrestha@unm.edu>
2024-01-29 21:18:34 -0700

    updated Categories screen file

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,32 @@
- import CategoryGridTile from '../components/CategoryGridTile';
-import {CATEGORIES} from '../data/dummy-data';
-import { FlatList } from 'react-native';
- 
-  function renderCategoryItem(itemData){
-    return(
-        <CategoryGridTile title={itemData.item.title} color={itemData.item.id}/>
-    )
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+
+
+
+function CategoriesScreen({navigation}) {
+
+  function renderCategoryItem(itemData) {
+
+    function pressHandler(){
+        navigation.navigate('Details of food');
+    }
+
+
+    return (
+      <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress = {pressHandler}/>
+    );
   }
 
 
- function CategoriesScreen(){
-  return(
- <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem}
-  numColumns={2}/>
-  )
- }
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
 
-  export default CategoriesScreen; 
+export default CategoriesScreen;
