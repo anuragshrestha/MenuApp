ccf02a78f14df8db9b02f66daee5f34b8fb0b7a9
Anurag Shrestha <ashrestha@unm.edu>
2024-02-07 01:05:12 -0700

    Added redux for app wide state management

---
+++ store/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import favorite from "./favorite";
+
+
+ export const store = configureStore({
+    reducer: {
+        favoriteMeal : favorite
+    }
+ });
